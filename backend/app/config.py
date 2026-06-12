3da7ed1296b3270f50446f31ed17bf6e726fbddb
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-06-12 19:25:59 +0300

    add gemini api key

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,3 +7,5 @@ JIRA_BASE_URL = os.getenv("JIRA_BASE_URL")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
 JIRA_PROJECT_KEY = os.getenv("JIRA_PROJECT_KEY")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
