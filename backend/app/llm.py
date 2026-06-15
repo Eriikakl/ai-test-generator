06d3853f2887251d6f3c90de0655bd00aed59824
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-06-15 18:10:22 +0300

    improve LLM integration

--- backend/app/llm.py
+++ backend/app/llm.py
@@ -1,13 +1,22 @@
-## from app.mock_llm import MockLLM
+from app.mock_llm import MockLLM
 from google import genai
 import json
 
 class LLMService:
-    def __init__(self, api_key: str):
-      ##  self.engine = MockLLM()
-      self.client = genai.Client(api_key=api_key)
+    def __init__(self, api_key: str = None, use_mock: bool = False):
+
+      self.use_mock = use_mock 
+
+      if use_mock:
+         self.engine = MockLLM()
+      else:
+        self.client = genai.Client(api_key=api_key)
 
     def generate(self, prompt: str) -> dict:
+        
+        if self.use_mock: 
+           return self.engine.generate(prompt)
+        
         response = self.client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt
